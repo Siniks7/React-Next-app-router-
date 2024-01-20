87d7ad7b505f3029976ee4d9ca1948ad82f9894b
siniks7 <siniks7@yandex.ru>
2024-01-21 00:43:48 +0700

    fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
-import Head from 'next/head';
+
 
 export default function Home() {
-	return (<>
-		<Head>
-			<title>Next</title>
-			<meta property="og:title" content="My page title" key="title" />
-		</Head>
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-		
-			<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-				
-				<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-					<code className="font-mono font-bold">src/app/page.tsx</code>
-				</p>
+	return (
+		<>
+			<div className=" font-medium text-[26px]">
+				Курсы по Photoshop
 			</div>
-		</main>
-	</>
+		</>
 	);
 }
