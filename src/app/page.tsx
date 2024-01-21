35dad6a016ca19a4c5f251c1e88f4474ed77f14c
siniks7 <siniks7@yandex.ru>
2024-01-21 16:52:10 +0700

    Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Htag } from '../components';
+import { Button, Htag } from '../components';
 
 export default function Home() {
 	return (
-		<Htag tag='h1'>Курсы по Photoshop</Htag>
+		<>
+			<Htag tag='h1'>Курсы по Photoshop</Htag>
+			<Button appearance='primary'>Кнопка</Button>
+			<Button appearance='ghost'>Кнопка</Button>
+		</>
 	);
 }
